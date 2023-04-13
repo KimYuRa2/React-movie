61dd0105d95feb94e7d3d5bb3a8d5b5b0ef58857
urakim <urakim@ex-it.co.kr>
2023-04-13 17:45:19 +0900

    router추가

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
+// 코드 Home.js로 옮기고 => App.js에는 라우터 관련 코드 작성
+
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  // getMovies = async () => {
-  //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json"); // axios를 통해 요청
-  //   console.log(movies.data.data.movies); // List-movies.json에서 필요한정보만 가져옴 (영화데이터리스트)
-  // };
-  getMovies = async () => {
-    // 위 코드를 정리함
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    // console.log(movies);
-
-    // this.setState({ movies : movies });
-    this.setState({ movies, isLoading: false }); // 위 코드를 축약 (ES6) + axios 요청 완료시 - isLoading : false로 설정
-  };
-
-  componentDidMount() {
-    this.getMovies(); // 처음 render시,  ComponentDidMount 실행 => getMovies() 실행됨.
-  }
-
-  render() {
-    const { movies, isLoading } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading ...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,63 @@
+// 기존 App.js
+
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  // getMovies = async () => {
+  //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json"); // axios를 통해 요청
+  //   console.log(movies.data.data.movies); // List-movies.json에서 필요한정보만 가져옴 (영화데이터리스트)
+  // };
+  getMovies = async () => {
+    // 위 코드를 정리함
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies);
+
+    // this.setState({ movies : movies });
+    this.setState({ movies, isLoading: false }); // 위 코드를 축약 (ES6) + axios 요청 완료시 - isLoading : false로 설정
+  };
+
+  componentDidMount() {
+    this.getMovies(); // 처음 render시,  ComponentDidMount 실행 => getMovies() 실행됨.
+  }
+
+  render() {
+    const { movies, isLoading } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading ...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
